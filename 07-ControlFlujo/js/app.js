6b5b32e474fab4a1bdd746431300aa8e6860dbf0
adrianla117 <alopeza23@fpcoruna.afundacion.org>
2024-10-14 13:18:19 +0200

    Ejercicio 1

--- 07-ControlFlujo/js/app.js
+++ 07-ControlFlujo/js/app.js
@@ -76,3 +76,124 @@ console.log((local > visitante) ? "Local gana" :
 
 
 //document.write("<h1>dadd</h1>"); Así ponemos etiquetas en js
+
+
+//OBJETO DATE
+/*Objeto que permite crear y manipular fechas y horas
+    No es estático, tengo que crearlo con new
+    sintaxis:
+        const fecha = new Date();
+
+    Métodos:
+        getFullYear(), getDate(), getDay(), getHours(), getMinutes(), getMonth(), getTime()
+*/
+const fecha = new Date(); //Le puse const porque el valor de fecha no va a cambiar.
+console.log(fecha);
+console.log(fecha.getFullYear());
+console.log(fecha.getHours());
+console.log(fecha.getDate());
+console.log(fecha.getDay()); //La semana empieza en 0 que es domingo
+console.log(fecha.getTime()); //devuelve la fecha en ms
+
+//creamos dos fechas 1/1/2024 31/12/2024
+const fecha1 = new Date("2024-1-1");
+console.log(fecha1);
+//Segunda forma de construírla:
+//const fecha2 = new Date(2024,0,1); //Enero empieza en el 0
+const fecha2 = new Date("2024-12-31");
+let diferenciaFecha = fecha2.getTime() - fecha1.getTime();
+let diferenciaDias = diferenciaFecha /(1000 * 60 * 60 * 24);
+console.log(Math.round(diferenciaDias));
+
+//SWITCH
+/*
+    Sintaxis:
+        switch (expresion){
+            case valor1:
+                instrucciones;
+                break;
+            case valor2:
+                instrucciones;
+                break;
+            default: //opcional
+                instrucciones;
+                //no hace falta break;
+        }
+
+        Tiene comparaciones estrictas!!! === !==    == != (no estricta compara el valor solo y estricta el valor y el tipo)
+*/
+
+let nota = 3;
+switch (nota){
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+        console.log("Suspenso");
+        break;
+    case 5:
+        console.log("Aprobado");
+        break;
+    case 6:
+        console.log("Bien");
+        break;
+    case 7:
+    case 8:
+        console.log("Notable");
+        break;
+    case 9:
+    case 10:
+        console.log("Sobresaliente");
+        break;
+    default:
+        console.log("La nota es incorrecta");
+}
+
+
+//FOR
+/*
+    Sintaxis:
+        for (expresionInicial; expresionCondicion; expresionActualizacion){
+            instrucciones;
+        }
+
+*/
+
+for (let i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+//console.log(i);
+
+//OMITIR PRIMERA EXPRESIÓN
+let i = 1;
+for ( ; i<=10; i++){
+    console.log(i);
+}
+console.log(i); //11
+
+//OMITIR SEGUNDA EXPRESIÓN
+for (let j = 1; ; j++){
+    if(j>10){
+        break;
+    }
+    console.log(j);
+}
+
+//OMITIR TERCERA INSTRUCCIÓN
+for (let j = 1; j<=10; ){
+    console.log(j);
+    j++; //j += 1;
+}
+
+//continue
+for (let j = 1; j <= 10; j++){
+    if (j%2===0){
+        continue; //continue salta a la iteración siguiente
+    }
+    if (j>5){
+        break; //break termina la ejecución
+    }
+    console.log(j);
+}
+
--- 07-ControlFlujo/js/ej1.js
+++ 07-ControlFlujo/js/ej1.js
@@ -1,3 +1,12 @@
 let edad = prompt("Introduzca su edad");
 console.log(edad);
 
+if(!edad){
+    console.log("Debes escribir una edad");
+}else if(edad === null){
+        console.log("Le has dado a cancelar");
+}else if(isNaN(edad)){
+    console.log("No has introducido un número");
+}else{
+    console.log("Tu edad es"+edad);
+}
